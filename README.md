29334f99fe3e31d809e719bb3d89d123d46761fb
Tobias Reimann <info@tobias-reimann.com>
2023-04-12 15:26:37 +0200

    Chapter 1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Effiziente Algortihmen in der Computergrafik
+
+Dieses Repository enthält den Code der Vorlesung "Effiziente Algorithmen in der Computergrafik" an der Technischen Hochschule Mittelhessen.
+
+## Inhalt
+
+- /cgx_01/ - Code von Kapitel 1
+- /cgx_02/ - Code von Kapitel 2
+- /cgx_03/ - ...
+- /libraries/ - Bibliotheken, die zum Kompilieren benötigt werden
+
+
+## Projekt starten
+
+Jedes Kapitel enthält ein eigenes `Makefile`. Wechseln Sie in das jeweilige Verzeichnis und führen Sie `make` bzw `make run` aus.
+
+```
+$ cd cgx_01
+$ make run
+```
+
+
+## Aufgabenstellung
+
+Die Beschreibung der einzelnen Kapitel finden Sie entweder direkt hier im Repository unter `/cgx_01/cgx_01.trj` oder auf [meiner Website](https://www.tobias-reimann.com/thm/effiziente-algorithmen-in-der-computergrafik/).
--- cgx_01/src/main.cpp
+++ cgx_01/src/main.cpp
@@ -1,21 +1,27 @@
+/**
+ * Effiziente Algorithmen in der Computergrafik
+ * Copyright (C) 2023 Tobias Reimann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 
 #include "game.h"
 #include "graphics.h"
 #include "settings.h"
 #include "window.h"
-
-// void* operator new(size_t size)
-// {
-//     //std::cout << "Allocating " << size << " bytes." << std::endl;
-//     return malloc(size);
-// }
-
-// void operator delete(void *memory, size_t zise)
-// {
-//     std::cout << "Freeingbytes." << std::endl;
-//     free(memory);
-// }
 
 int main()
 {
