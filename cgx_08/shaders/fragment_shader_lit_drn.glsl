7cf14aa1029ce73dba2a963f5a8297424d7a6f74
Tobias Reimann <info@tobias-reimann.com>
2023-06-16 14:04:53 +0200

    Updates

--- cgx_08/shaders/fragment_shader_lit_drn.glsl
+++ cgx_08/shaders/fragment_shader_lit_drn.glsl
@@ -11,11 +11,13 @@ uniform sampler2D Diffuse;
 uniform sampler2D Roughness;
 uniform sampler2D NormalMap;
 
+uniform samplerCube CubeMap;
+
 void main()
 {
     vec3 lightColor = vec3(1.0, 1.0, 1.0);
-    vec3 albedo = vec3(texture(Diffuse, TexCoord));
-    float roughness = vec3(texture(Roughness, TexCoord)).r;
+    vec3 albedo = pow(texture(Diffuse, TexCoord).rgb, vec3(2.2));
+    float roughness = texture(Roughness, TexCoord).r;
     vec3 normal = normalize(texture(NormalMap, TexCoord).rgb * 2.0 - 1.0);
 
     float lightIntensity = max(dot(normal, -SunLightObjSpc), 0.0);
@@ -24,8 +26,11 @@ void main()
     vec3 diffuse = 0.9 * lightIntensity * lightColor * albedo;
 
     vec3 viewDir = normalize(VertPos - CameraPosObjSpc);
-    vec3 reflectDir = reflect(-SunLightObjSpc, normal);
-    vec3 specular = 0.5 * pow(max(dot(viewDir, reflectDir), 0.0), 16) * lightColor * (1.0 - roughness);
+    vec3 reflectDir = reflect(viewDir, normal);
+    vec3 reflection = pow(texture(CubeMap, reflectDir).rgb, vec3(2.2)) * (1.0 - roughness);
 
-    FragColor = vec4(ambient + diffuse + specular, 1.0);
+    FragColor = vec4(ambient + diffuse + reflection, 1.0);
+
+    //gamma correction
+    FragColor = vec4(pow(FragColor.rgb, vec3(1.0/2.2)), FragColor.a);
 }
